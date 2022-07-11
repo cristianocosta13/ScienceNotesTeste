581100e0017b47302dca351543145d8830c692a8
cristianocosta13 <103438234+cristianocosta13@users.noreply.github.com>
2022-07-11 17:02:17 -0300

    Update questao.dart

--- lib/domínio/questao.dart
+++ lib/domínio/questao.dart
@@ -16,7 +16,7 @@ class Questao {
     required this.alt1,
     required this.alt2,
     required this.alt3,
-    required this.alt4,
+    required this.alt4, 
     required this.alt5,
     //required this.corConteudo,
     //required this.corConteudo,
